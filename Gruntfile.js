43428979d83910a6659cc989673eb19084100102
llucaso <lredzinski@gmail.com>
2017-06-09 10:43:06 +0200

    core

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+﻿module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    sass: {
+        options: {
+            sourceMap: true
+        },
+        dist: {
+            files: {
+                'css/main.css': 'sass/main.sass'
+            }
+        }
+    },
+
+    watch: {
+        scripts: {
+            files: ['sass/*.sass'],
+            tasks: ['sass'],
+            options: {
+                spawn: false,
+            },
+        }
+    },
+
+    browserSync: {
+            dev: {
+                bsFiles: {
+                    src : [
+                        'css/*.css',
+                        '*.html'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: './'
+                }
+            }
+        }
+
+
+  });
+
+  //Load the plugins tasks
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
+
+
+  //Default task(s).
+  grunt.registerTask('default', ['sass', 'browserSync', 'watch']);
+
+};
